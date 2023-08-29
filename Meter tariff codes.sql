c06be9511d975d15d5c568d628d9f321e3db34cd
Maysolanki81 <140139042+Maysolanki81@users.noreply.github.com>
2023-08-29 13:56:08 +1000

    Delete Meter tariff codes.sql

--- Meter tariff codes.sql
+++
@@ -1,8 +0,0 @@
-select transaction_sub_type, rate, line_description, transaction_type 
-from net_transaction
-where net_id = '1025'
-group by transaction_sub_type, rate,  line_description, transaction_type 
-
-select meter_id, transaction_sub_type, rate, line_description, transaction_type 
-from net_transaction
-where net_id = '1025'
